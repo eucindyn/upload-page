d5c27a37a8b0b517dee8a57648bda69f12fde808
eucindyn <mcindynascimento@gmail.com>
2023-06-20 12:27:36 -0300

    :sparkles: feat: removing event

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const dropArea = document.querySelector(".drag-files");
 dropArea.addEventListener("dragover", () => {
   dropArea.classList.add("dragover");
 });
+
+dropArea.addEventListener("dragleave", () => {
+  dropArea.classList.remove("dragover");
+});
